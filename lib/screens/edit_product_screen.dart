67d59991842ddd999fa348241f93b0a28a601f97
felix-kit-dev <felix.kithuka@gmail.com>
2024-08-04 12:05:35 +0300

    Create edit_product_screen.dart

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: Center(
+        child: Text('Edit Product Screen'),
+      ),
+    );
+  }
+}
