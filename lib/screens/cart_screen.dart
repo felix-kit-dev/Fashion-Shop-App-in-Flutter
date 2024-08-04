23e7f4de746b4609eb446fd172aca1e71487fb6c
felix-kit-dev <felix.kithuka@gmail.com>
2024-08-04 12:02:52 +0300

    Create cart_screen.dart

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../screens/checkout_screen.dart';
+import '../providers/cart.dart';
+import '../widgets/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total', style: TextStyle(fontSize: 20)),
+                  Spacer(),
+                  Chip(
+                    label: Text('\$${cart.totalAmount.toStringAsFixed(2)}'),
+                  ),
+                  TextButton(
+                    child: Text('ORDER NOW'),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(CheckoutScreen.routeName);
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItemWidget(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
