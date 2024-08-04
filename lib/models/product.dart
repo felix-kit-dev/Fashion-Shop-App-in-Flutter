c6402cb44fc3feff15ec6a5428249b4dad386586
felix-kit-dev <felix.kithuka@gmail.com>
2024-08-04 11:59:57 +0300

    Create product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String title;
+  final double price;
+  final String description;
+  final String imageUrl;
+  final String category;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+    required this.category,
+  });
+}
